2dc29ac58404ce35d6e26627a8c49029d3246de1
Your Name <you@example.com>
2026-03-03 18:17:31 +0800

    added index.ts on server

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.json({ message: 'TriPPLesPH API running 🚀' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
