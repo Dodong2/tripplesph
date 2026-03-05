0849ac1577062599c31679555352aab1c5b09b37
Your Name <you@example.com>
2026-03-05 17:41:27 +0800

    change orm drizzle to prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import Redis from 'ioredis'
-import { Pool } from "pg";
+import { Redis } from 'ioredis'
+import prisma from './db/prisma.js'
 
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({ message: 'TriPPLesPH API running 🚀' })
@@ -21,23 +23,38 @@ redis.get("test_key").then((value) => {
     console.log("✅ Redis connected:", value)
 })
 
-// postgres testing connection
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
+// prisma postgres connection test
 async function testPostgres() {
   try {
-    console.log("⏳ Testing Postgres connection...");
-    const res = await pool.query("SELECT NOW()");
-    console.log("✅ Postgres connected:", res.rows[0]);
+    console.log('⏳ Testing Postgres connection...')
+    await prisma.$connect()
+    console.log('✅ Postgres connected via Prisma') 
   } catch (err) {
-    console.error("❌ Postgres connection failed:");
-    console.error(err);
+    console.error('❌ Postgres connection failed:', err)
   }
 }
 
-testPostgres();
+testPostgres()
+
+
+//test API connection backend
+app.get('/db-test', async (req, res) => {
+  try {
+    const test = await prisma.testConnection.create({
+      data: { message: 'Prisma database working connected 🚀' }
+    })
+    res.json({
+      status: 'success',
+      data: test
+    })
+  } catch(err: any) {
+    res.status(500).json({
+      statu: "error",
+      message: err.message,
+      code: err.mode
+    })
+  }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
