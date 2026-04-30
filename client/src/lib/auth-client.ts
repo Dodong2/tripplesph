c66d2b0969718abb64b5aa6beb630214a4557345
Your Name <you@example.com>
2026-04-30 12:30:55 +0800

    fix(client) set-up netlify testing deployment

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: '',
     fetchOptions: {
-        credentials: "include"
+        credentials: 'include'
     }
 })
