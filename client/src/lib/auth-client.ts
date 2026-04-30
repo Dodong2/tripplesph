ad96eeb73d941412784375d6170932f8ffb0b761
Your Name <you@example.com>
2026-04-30 15:08:36 +0800

    fix(client) set-up render testing

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: '',
+    baseURL: import.meta.env.VITE_API_URL,
     fetchOptions: {
-        credentials: 'include'
+        credentials: "include"
     }
 })
